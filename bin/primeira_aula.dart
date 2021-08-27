94b58a93efc9c5d85313523c7f8b539ec0fbfda2
LG61 <luancordeiro732@gmail.com>
2021-08-27 19:17:28 -0300

    revisão - variáveis e estrutura de decisão

--- bin/primeira_aula.dart
+++ bin/primeira_aula.dart
@@ -2,16 +2,22 @@ import 'package:primeira_aula/primeira_aula.dart' as primeira_aula;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Informe o seu nome:');
-  String nome = stdin.readLineSync() ?? '';
+ print('Qual o seu nome?');
+ var nome = stdin.readLineSync();
+ print('olá ${nome}, bom dia!');
 
-  print('Informe a sua idade:'); 
-  String entradaidade = stdin.readLineSync() ?? '';
-  var idade = int.parse(entradaidade);
-  
-  print('Informe a seu salário:'); 
-  String entradasalario = stdin.readLineSync() ?? '';
-  var salario = double.parse(entradasalario);
-  
-  print('Nome:$nome, idade:$idade, mesada:$salario');
+ print('Você como programador(a), recebe quanto de salário?');
+ var textsalario = stdin.readLineSync();
+ var salario = double.parse(textsalario!);
+ if(salario > 2774 ){
+   print('Você recebe mais que a média! deve ser um(a) otimo(a) programador(a)!');
+ } 
+ else{
+   print('É menos que a média.');
+   if(salario < 1100){
+     print('Acho até que você esta sendo roubado.');
+   }
+ }
+
+
 }
